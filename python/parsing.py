eab53c0883b161431f9ae5b7ea2e25704cf8a73c
Murali Suriar <code@suriar.com>
2015-07-11 20:58:50 +0100

    Add docstrings, disable public method checks for Character.

--- python/parsing.py
+++ python/parsing.py
@@ -1,4 +1,9 @@
+"""Module parsing contains classes used to implement a basic parser."""
+
+
 class Character(object):
+    """Class Character is a data object for a single character."""
+    # pylint: disable-msg=too-few-public-methods
     def __init__(self, index, line, column, text):
         self.payload = text[index]
         self.index = index
