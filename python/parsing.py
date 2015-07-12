bc579d75428354d7e146d5123ce04fdcc79a6f73
Murali Suriar <code@suriar.com>
2015-07-12 13:53:46 +0100

    First passing scanner test.

--- python/parsing.py
+++ python/parsing.py
@@ -10,3 +10,20 @@ class Character(object):
         self.line = line
         self.column = column
         self.text = text
+
+    def __eq__(self, other):
+        return self.__dict__ == other.__dict__
+
+
+class Scanner(object):
+    """Class Scanner is a scanner which reads in source text and then returns
+    one character at a time in response to the 'get()' method."""
+
+    def __init__(self, text):
+        self.text = text
+        self.index = 0
+        self.line = 0
+        self.column = 0
+
+    def get(self):
+        return Character(self.index, self.line, self.column, self.text)
--- python/parsing_test.py
+++ python/parsing_test.py
@@ -1,11 +1,27 @@
+"""Tests for module 'parsing'."""
+
 import parsing
 
 import unittest
 
+
 class ScannerTest(unittest.TestCase):
-    pass
+    """Unittests for Scanner class."""
+    def test_single_character(self):
+        text = 'b'
+        testdata = [
+            dict(index=0, line=0, column=0, text=text),
+            ]
+
+        s = parsing.Scanner(text=text)
+        for i in range(len(text)):
+            c = s.get()
+            expected = parsing.Character(**testdata[i])
+            self.assertEqual(c, expected)
+
 
 class CharacterTest(unittest.TestCase):
+    """Unittests for Character class."""
     def test_single_character(self):
         payload = 'a'
         index = 0
